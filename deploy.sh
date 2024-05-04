6b1a55bc5ad4570c6802f8b2da5cb083a7869ea6
ppp <philipp.panhey@gmail.com>
2024-05-04 08:40:53 +0200

    chore: added hallo welt

--- deploy.sh
+++ deploy.sh
@@ -4,25 +4,11 @@
 source .env
 
 # Check if the variables are set
-if [ -z "$REMOTE_SERVER_IP" ] || [ -z "$REMOTE_SERVER_USER" ]; then
+if [ -z "$REMOTE_SERVER_IP" ] || [ -z "$REMOTE_SERVER_USER" ] || [ -z "$REMOTE_SERVER_DIR" ]; then
   echo "One or more environment variables are missing. Make sure to set them in the .env file."
   exit 1
 fi
 
-
-ssh ${REMOTE_SERVER_USER}@${REMOTE_SERVER_IP} "cd simple_website/;sh docker_redeploy.sh"
-
-
-# Transfer index.html
-# scp index.html ${REMOTE_SERVER_USER}@${REMOTE_SERVER_IP}:${REMOTE_SERVER_DIR}
-
-# # Transfer favicon
-# scp favicon.ico ${REMOTE_SERVER_USER}@${REMOTE_SERVER_IP}:${REMOTE_SERVER_DIR}
-
-# # Transfer css
-# scp style.css ${REMOTE_SERVER_USER}@${REMOTE_SERVER_IP}:${REMOTE_SERVER_DIR}
-
-# # Transfer img/ directory
-# scp -r img/ ${REMOTE_SERVER_USER}@${REMOTE_SERVER_IP}:${REMOTE_SERVER_DIR}
+ssh ${REMOTE_SERVER_USER}@${REMOTE_SERVER_IP} "cd ${REMOTE_SERVER_DIR}; sh docker_redeploy.sh"
 
 echo "Deployment complete."
