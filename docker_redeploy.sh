3fb615559ec1cda949caafbd1a7a4c21d3e0875a
ppp <philipp.panhey@gmail.com>
2024-05-04 08:26:19 +0200

    feat: added redeploy script

---
+++ docker_redeploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Pull the latest changes from GitHub
+git pull origin main
+
+# Build the new Docker image
+docker build -t philipp-panhey-de .
+
+# Stop the currently running container
+docker stop philipp-panhey-de || true
+
+# Remove the stopped container
+docker rm philipp-panhey-de || true
+
+# Run the new Docker container
+docker run -d --name philipp-panhey-de -p 80:80 -p 443:443 philipp-panhey-de
